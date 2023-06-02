251cb32f98761432ec3c3ec2f848f63b08af668a
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-06-02 17:00:05 +0200

    done

--- models/contacts.js
+++ models/contacts.js
@@ -18,17 +18,37 @@ async function getContactById(contactId) {
 async function removeContact(contactId) {
   const data = await listContacts();
   const index = data.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
   const contact = data.splice(index, 1);
   fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
   return contact;
 }
 
-async function addContact({name, email, phone}) {
+async function addContact(data) {
   const contacts = await listContacts();
-  const newContact = { id: nanoid(), name, email, phone };
+  const newContact = { id: nanoid(), ...data };
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return newContact;
 }
 
-module.exports = { listContacts, addContact, getContactById, removeContact };
+async function editContactById(id, data) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id, ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+}
+
+module.exports = {
+  listContacts,
+  addContact,
+  getContactById,
+  removeContact,
+  editContactById,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -32,7 +32,7 @@ router.get("/:id", async (req, res, next) => {
       // error.status = 404;
       // throw error;
     }
-    return res.json(result);
+    res.json(result);
   } catch (error) {
     next(error);
   }
@@ -40,15 +40,45 @@ router.get("/:id", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const { error } = addSchema.validate(req.body)
+    const { error } = addSchema.validate(req.body);
     if (error) {
-     throw HttpError(400, error.message)
-   }
+      throw HttpError(400, error.message);
+    }
     const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
+   res.status(201).json(result);
   } catch (error) {
     next(error);
   }
 });
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const data = req.body;
+    const result = await contacts.editContactById(id, data);
+    if (!result) {
+      throw HttpError(404, "not found");
+    }
+    return res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+  const {id} = req.params
+    const result = await contacts.removeContact(id)
+    if (!result) {
+      throw HttpError(404,'not found')
+    }
+    res.json({message:'delete success'})
+} catch (error) {
+  next(error)
+}})
 
 module.exports = router;
