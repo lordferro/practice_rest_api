a5384be76c1ef83d255c7b399facc19b6b1a57b9
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-06-02 12:59:04 +0200

    add error check with Joi

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,34 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
-
-const removeContact = async (contactId) => {}
-
-const addContact = async (body) => {}
-
-const updateContact = async (contactId, body) => {}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
 }
+
+async function getContactById(contactId) {
+  const data = await listContacts();
+  const contact = data.find((item) => item.id === contactId);
+  return contact || null;
+}
+
+async function removeContact(contactId) {
+  const data = await listContacts();
+  const index = data.findIndex((item) => item.id === contactId);
+  const contact = data.splice(index, 1);
+  fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+  return contact;
+}
+
+async function addContact({name, email, phone}) {
+  const contacts = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+module.exports = { listContacts, addContact, getContactById, removeContact };
